1755aec75554d53f6579d2700b92a103f713f315
Костин Максим Романович <MRKostin@sberbank.ru>
2021-11-05 01:05:46 +0300

    Added validation

---
+++ src/main/java/com/example/barbershop/models/Report.java
@@ -0,0 +1,61 @@
+package com.example.barbershop.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.math.BigInteger;
+
+@Entity
+public class Report {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private BigInteger id;
+    @NotEmpty(message = "Заполните поле с именем")
+    @Size(min = 2, max = 50, message = "Имя должно быть больше 2 и меньше 50 символов")
+    private String name;
+    @NotEmpty(message = "Заполните поле с email")
+    @Email(message = "Невалидный email")
+    private String email;
+    @NotEmpty(message = "Заполните поле с темой")
+    @Size(min = 2, max = 50, message = "Тема должна быть больше 2 и меньше 50 символов")
+    private String subject;
+    @NotEmpty(message = "Заполните поле с сообщением")
+    @Size(min = 2, message = "Сообщение должно быть больше 2 символов")
+    private String message;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
