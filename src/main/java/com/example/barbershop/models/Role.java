69159736b9c57367bcc5edeaa96647c061524af0
Костин Максим Романович <MRKostin@sberbank.ru>
2021-11-07 23:16:45 +0300

    added authentication

---
+++ src/main/java/com/example/barbershop/models/Role.java
@@ -0,0 +1,5 @@
+package com.example.barbershop.models;
+
+public enum Role {
+    ADMIN,USER
+}
---
+++ src/main/java/com/example/barbershop/models/User.java
@@ -0,0 +1,64 @@
+package com.example.barbershop.models;
+
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.math.BigInteger;
+
+@Entity
+@Table(name = "usr")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private BigInteger id;
+    private String username;
+    private String password;
+    private boolean active;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public BigInteger getId() {
+        return id;
+    }
+
+    public void setId(BigInteger id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
